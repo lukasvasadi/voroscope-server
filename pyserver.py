04c60cf6a99d1eba5c1742d388de72af187a74e0
lukasvasadi <lukas.vasadi@gmail.com>
2023-02-22 11:09:29 -0500

    Improve socket closed error handling

--- pyserver.py
+++ pyserver.py
@@ -10,6 +10,7 @@ from typing import Callable
 from multiprocessing import Process
 from argparse import ArgumentParser
 from websockets.server import WebSocketServerProtocol
+from websockets.exceptions import ConnectionClosedError
 
 
 # Setup optional command line arguments
@@ -30,47 +31,57 @@ STAGEPORT = args.stageport if args.stageport else defaults["stageport"]
 
 async def handle_camera(socket: WebSocketServerProtocol, camera: Camera):
     camera = Camera()
-    async for message in socket:
-        instruction: dict = json.loads(
-            message
-        )  # Convert json serialized message to dict
-        for key in instruction.keys():
-            match key:
-                case CameraKey.CFG.value:
-                    camera.resolution = tuple(instruction[key]["resolution"])
-                    await camera.startup()
+    try:
+        async for message in socket:
+            instruction: dict = json.loads(
+                message
+            )  # Convert json serialized message to dict
+            for key in instruction.keys():
+                match key:
+                    case CameraKey.CFG.value:
+                        camera.resolution = tuple(instruction[key]["resolution"])
+                        await camera.startup()
 
-                    # NOTE: Tasks can be cancelled manually!
-                    task = asyncio.create_task(camera.get_frames(socket))
-                    # await task
+                        # NOTE: Tasks can be cancelled manually!
+                        task = asyncio.create_task(camera.get_frames(socket))
+                        # await task
 
-                    # if task.exception():
-                    #     print(f"Warning: Camera raised {task.exception()}")
-                case _:
-                    await socket.send(
-                        json.dumps({"err": f"Unrecognized instruction: {key}"})
-                    )
-    camera.close()
+                        # if task.exception():
+                        #     print(f"Warning: Camera raised {task.exception()}")
+                    case _:
+                        await socket.send(
+                            json.dumps({"err": f"Unrecognized instruction: {key}"})
+                        )
+    except ConnectionClosedError:
+        pass
+    finally:
+        camera.close()
 
 
 async def handle_stage(socket: WebSocketServerProtocol, stage: Stage):
     stage = Stage()
-    async for message in socket:
-        instruction: dict = json.loads(
-            message
-        )  # Convert json serialized message to dict
-        for key in instruction.keys():
-            match key:
-                case StageKey.POS.value:
-                    asyncio.create_task(stage.get_position(socket, instruction[key]))
-                case StageKey.CMD.value:
-                    print(instruction[key])
-                    await stage.send(instruction[key])
-                case _:
-                    await socket.send(
-                        json.dumps({"err": f"Unrecognized instruction: {key}"})
-                    )
-    stage.close()
+    try:
+        async for message in socket:
+            instruction: dict = json.loads(
+                message
+            )  # Convert json serialized message to dict
+            for key in instruction.keys():
+                match key:
+                    case StageKey.POS.value:
+                        asyncio.create_task(
+                            stage.get_position(socket, instruction[key])
+                        )
+                    case StageKey.CMD.value:
+                        print(instruction[key])
+                        await stage.send(instruction[key])
+                    case _:
+                        await socket.send(
+                            json.dumps({"err": f"Unrecognized instruction: {key}"})
+                        )
+    except ConnectionClosedError:
+        pass
+    finally:
+        stage.close()
 
 
 def cfg_websocket(handle: Callable, address: str, port: int):
